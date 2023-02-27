367a2bfb6bb125ac54cb1cdf742a976f5231ea52
Andrei <nicolaev.andrei09@gmail.com>
2023-02-27 15:30:51 +0200

    commit 5

--- script.js
+++ script.js
@@ -4,12 +4,19 @@ function selected() {
     element.classList.remove("selected");
   else element.classList.add("selected");
 }
-function toggleNavBar(){
+function toggleNavBar() {
   const ul = document.querySelector("ul");
-  if(ul.style.display==="none")
-    ul.style.display="flex"
-    else
-    ul.style.display="none"
+  var menu = document.querySelector("p");
+  if (ul.style.display === "none") {
+    ul.style.display = "flex";
+    menu.innerText = "Close";
+    menu.style.position="fixed"
+    menu.style.right="4rem"
+  } else {
+    ul.style.display = "none";
+    menu.innerText = "menu";
+    menu.style.position="static"
+  }
 }
 function addSideNavbarClass() {
   const windowSize = window.innerWidth;
@@ -19,15 +26,16 @@ function addSideNavbarClass() {
 
   if (windowSize <= 768) {
     ul.classList.add("side-navbar");
-    title.style.display="none";
-    ul.style.display="none";
-    menu.style.display="block";
+    title.style.display = "none";
+    ul.style.display = "none";
+    menu.style.display = "block";
   } else {
     ul.classList.remove("side-navbar");
-    ul.style.display="flex";
-    title.style.display="block";
-    menu.style.display="none";
+    ul.style.display = "flex";
+    title.style.display = "block";
+    menu.style.display = "none";
   }
 }
+window.addEventListener("click", selected);
 window.addEventListener("resize", addSideNavbarClass);
 window.addEventListener("load", addSideNavbarClass);
