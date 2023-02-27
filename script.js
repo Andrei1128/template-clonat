36d74ec2feaf4c6b2994dfeaa98dc3ac7f0a73eb
Andrei <nicolaev.andrei09@gmail.com>
2023-02-27 15:15:02 +0200

    commit 4

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ function selected() {
     element.classList.remove("selected");
   else element.classList.add("selected");
 }
+function toggleNavBar(){
+  const ul = document.querySelector("ul");
+  if(ul.style.display==="none")
+    ul.style.display="flex"
+    else
+    ul.style.display="none"
+}
+function addSideNavbarClass() {
+  const windowSize = window.innerWidth;
+  const ul = document.querySelector("ul");
+  const title = document.querySelector("h3");
+  const menu = document.querySelector("p");
+
+  if (windowSize <= 768) {
+    ul.classList.add("side-navbar");
+    title.style.display="none";
+    ul.style.display="none";
+    menu.style.display="block";
+  } else {
+    ul.classList.remove("side-navbar");
+    ul.style.display="flex";
+    title.style.display="block";
+    menu.style.display="none";
+  }
+}
+window.addEventListener("resize", addSideNavbarClass);
+window.addEventListener("load", addSideNavbarClass);
